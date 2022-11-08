87df974ff5b07ee6fde8323e7db60fe943c5b07c
Elina Hovakimyan <elina.hovakimyan@firstvet.com>
2022-11-09 02:52:04 +0400

    Replace API Key

--- src/http/index.js
+++ src/http/index.js
@@ -6,7 +6,7 @@ export const getRecipesList = async (tags = null, size) => {
         url: 'https://tasty.p.rapidapi.com/recipes/list',
         params: { from: '0', size: size || '20', tags },
         headers: {
-            'X-RapidAPI-Key': '4c78c6cbc0msh689dccf56625cf1p117b84jsn897f105ecb99',
+            'X-RapidAPI-Key': 'YOUR_API_KEY',
             'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
         }
     };
